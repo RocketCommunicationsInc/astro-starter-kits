f6cee34b57e96b3532fc4287601cc6279f6e6d89
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-06-20 12:50:11 -0400

    add angular

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AstroComponentsModule } from '@astrouxds/angular'
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, AstroComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
